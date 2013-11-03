f9f2802d45717f60fe5d66bd208388e3e90ea417
Giles Lavelle <giles.lavelle@gmail.com>
2013-11-03 17:01:28 +0000

    random image

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(function(){
+    var images = ['elephant'];
+
+    var randomImage = function(){
+        return "url('/img/" + images[Math.floor(Math.random() * images.length)] + ".jpg')"
+    };
+
     $('#banner').css({
-        'background-image': "url('/img/elephant.jpg')"
+        'background-image': randomImage()
     });
 });
