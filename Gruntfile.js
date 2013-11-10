2ae1776fdc0ca646a22a10c2813f98dd59183c0f
Giles Lavelle <giles.lavelle@gmail.com>
2013-11-10 19:16:43 +0000

    use stylus

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,18 +84,26 @@ module.exports = function(grunt) {
           'jst.js': ['templates/*.html']
         }
       }
+    },
+    stylus: {
+      compile: {
+        files: {
+          'css/style.css': 'stylus/style.styl'
+        }
+      }
     }
+
   });
 
   // These plugins provide necessary tasks.
   // grunt.loadNpmTasks('grunt-contrib-concat');
   // grunt.loadNpmTasks('grunt-contrib-uglify');
   // grunt.loadNpmTasks('grunt-contrib-qunit');
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
-  // grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jst');
 
   // Default task.
-  // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jst', 'stylus']);
 
 };
