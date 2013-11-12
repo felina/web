eea3801991f5d6864d45df94c0d130de243c0971
Giles Lavelle <giles.lavelle@gmail.com>
2013-11-12 15:32:28 +0000

    update dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,16 +4,16 @@ $(function(){
 
     var opts = [
         {
-            name: 'test',
-            eta: '2h 14m',
+            name: 'Process some penguins',
+            eta: '37m',
             current: 10,
             total: 37
         },
         {
-            name: 'test2',
+            name: 'Analyse some elephants',
             eta: '2h 15m',
-            current: 10,
-            total: 37
+            current: 82,
+            total: 96
         }
     ];
 
