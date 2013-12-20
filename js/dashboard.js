bf454dd279d72c968fab7f9375751a5b23bcb8bb
Giles Lavelle <giles.lavelle@gmail.com>
2013-12-20 18:43:10 +0000

    commented and refactored dashboard stuff

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,4 @@
 $(function(){
-    var tmpl = JST.job;
     var jobs = $('.jobs');
     var body = $('body');
     var toggler = $(JST.toggler());
@@ -11,31 +10,28 @@ $(function(){
     var currentWidth = sidebarWidth;
     var headerHeight = 60;
     var sidebarVisible = true;
+    var resizeDelay = 500;
 
+    // Add sidebar toggle button
     $('#header-left').append(toggler);
 
+    // Add settings modal
     body.append(JST.settings());
 
+    // TODO: swap this out for a real instance of the User class
     var user = {
         isResearcher: function(){ return true; }
     };
 
-    body.on('keypress', function(evt){
-        if(evt.keyCode === 116){
-            toggleSidebar();
-        }
-    });
-
-    toggler.click(function(){
-        toggleSidebar();
-    });
-
+    // Shows and hides the sidebar
     var toggleSidebar = function(){
         currentWidth = sidebarVisible ? 0 : sidebarWidth;
         setSidebar();
         sidebarVisible = !sidebarVisible;
     };
 
+    // Updates the dimensions and positions of the sidebar and dashboard to
+    // reflect the current state
     var setSidebar = function(){
         var height = body.height() - (headerHeight * 2);
         dashboard.css({
@@ -48,34 +44,39 @@ $(function(){
         });
     };
 
-    var opts = [
-        {
-            name: 'Process some penguins',
-            eta: '37m',
-            current: 10,
-            total: 37
-        },
-        {
-            name: 'Analyse some elephants',
-            eta: '2h 15m',
-            current: 82,
-            total: 96
+    // Bind events
+    // Keyboard shortcuts
+    body.on('keypress', function(evt){
+        // 't' toggles the sidebar
+        if(evt.keyCode === 116){
+            toggleSidebar();
         }
-    ];
+    });
 
-    for (var i = 0; i < opts.length; i++) {
-        jobs.append(tmpl(opts[i]));
-    };
+    // Clicking the toggle button also toggles the sidebar
+    toggler.click(function(){
+        toggleSidebar();
+    });
 
+    // Resizing the browser updates dashboard dimensions
+    $(window).resize(_.throttle(setSidebar, resizeDelay));
+
+    // Get data for the jobs and add it to the DOM
+    $.getJSON('data/jobs.json', function(data){
+        for (var i = 0; i < data.length; i++){
+            jobs.append(JST.job(data[i]));
+        };
+    });
+
+    // Add menu items to the sidebar
     if(user.isResearcher()){
         sidebarList.append(JST['sidebars/researcher']());
     }
-
     sidebarList.append(JST['sidebars/citizen']());
 
+    // Set initial sidebar dimensions
     setSidebar();
 
-    $(window).resize(_.throttle(setSidebar, 500));
-
+    // Load some dummy data and display it as a bar chart
     barChart('#graph', 'data/data.tsv')
 });
