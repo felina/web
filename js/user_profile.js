3d8dc57b55f843eea04ea9deec0c4ce9475e724c
Andy <narayn60@gmail.com>
2014-02-09 21:05:22 +0000

    Photos and headers sorted

--- js/user_profile.js
+++ js/user_profile.js
@@ -3,8 +3,8 @@ $(function(){
         username: 'Andrew Stuart',
         badgeCount: '10',
         mostUploaded: 'Penguins',
-        profile_picture: 'http://upload.wikimedia.org/wikipedia/commons/5/5b/Ultraviolet_image_of_the_Cygnus_Loop_Nebula_crop.jpg',
-        cover_photo: 'http://stylopics.com/wp-content/uploads/2013/08/fb_profile_cover_1317410583f25.jpg'
+        profile_picture: '/img/shutter.png',
+        cover_photo: '/img/leopard.jpg'
     }));
     var profileWrap = $('.profile_wrapper');
     profileWrap.append(profile);
@@ -16,7 +16,7 @@ $(function(){
     newsfeedWrap.append(newsfeed);
 
     var newsfeed_head = $(JST.newsfeed_head({
-        user_photo: 'http://upload.wikimedia.org/wikipedia/commons/e/e0/Anonymous.svg',
+        user_photo: '/img/shutter.png',
         posting_user: 'Peter Nincompoop',
         recieving_user: 'Andrew Stuart',
         time_post: 'Yesterday 16:06' 
@@ -24,15 +24,15 @@ $(function(){
     newsfeedWrap.find('.newshead_wrapper').append(newsfeed_head);
 
     var photos = [
-        'http://upload.wikimedia.org/wikipedia/commons/5/5b/Ultraviolet_image_of_the_Cygnus_Loop_Nebula_crop.jpg',
-        'http://upload.wikimedia.org/wikipedia/commons/5/5b/Ultraviolet_image_of_the_Cygnus_Loop_Nebula_crop.jpg',
-        'http://upload.wikimedia.org/wikipedia/commons/5/5b/Ultraviolet_image_of_the_Cygnus_Loop_Nebula_crop.jpg',
-        'http://upload.wikimedia.org/wikipedia/commons/5/5b/Ultraviolet_image_of_the_Cygnus_Loop_Nebula_crop.jpg',
-        'http://upload.wikimedia.org/wikipedia/commons/5/5b/Ultraviolet_image_of_the_Cygnus_Loop_Nebula_crop.jpg',
-        'http://upload.wikimedia.org/wikipedia/commons/5/5b/Ultraviolet_image_of_the_Cygnus_Loop_Nebula_crop.jpg',
-        'http://upload.wikimedia.org/wikipedia/commons/5/5b/Ultraviolet_image_of_the_Cygnus_Loop_Nebula_crop.jpg',
-        'http://upload.wikimedia.org/wikipedia/commons/5/5b/Ultraviolet_image_of_the_Cygnus_Loop_Nebula_crop.jpg',
-        'http://upload.wikimedia.org/wikipedia/commons/5/5b/Ultraviolet_image_of_the_Cygnus_Loop_Nebula_crop.jpg',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',        
     ];
 
 
@@ -44,31 +44,31 @@ $(function(){
     photo_wrapper.append(photoGallery);
 
     var badges = [
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
-        'http://upload.wikimedia.org/wikipedia/en/9/97/Upd_badge_edds_2007.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png',
+        '/img/shutter.png'
     ]
 
     var badgeGallery = $(JST.user_badges({
