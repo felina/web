5801492be5bc49c31ce35ca6d5b8b3aac59f17c3
Giles Lavelle <giles.lavelle@gmail.com>
2014-03-09 19:17:29 +0000

    call jobs endpoint

--- js/view_jobs.js
+++ js/view_jobs.js
@@ -2,9 +2,12 @@ $(function(){
     var jobs = $('.jobs');
 
     // Get data for the jobs and add it to the DOM
-    $.getJSON('data/jobs.json', function(data){
-        for (var i = 0; i < data.length; i++){
-            jobs.append(JST.job(data[i]));
+    fl.get({
+        url: 'jobs',
+        success: function(data){
+            for (var i = 0; i < data.length; i++){
+                jobs.append(JST.job(data[i]));
+            }
         }
     });
 
