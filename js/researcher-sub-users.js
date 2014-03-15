59f61383c3ea9486f66b1c60f7ee5fc69950b628
Andy <narayn60@gmail.com>
2014-03-15 16:33:19 +0000

    Have to have values to submit

--- js/researcher-sub-users.js
+++ js/researcher-sub-users.js
@@ -16,18 +16,19 @@ $(function(){
       var contents = $("#skinput").val();
       var name = $("#nameinput").val();
 
+      if (contents && name) { 
+        $('#addr'+i).html("<td>"+ i +"</td><td><input id='skcontainer"+i+"' name='serialkey"+i+"' type='text' placeholder='Serial Key' class='form-control input-md' disabled/> </td><td><input id='namecontainer"+i+"' name='name"+i+"' type='text' placeholder='Name'  class='form-control input-md' disabled></td> <td style='vertical-align:middle' align='center'><a class='btn btn-default'>Invalidate User</a><a class='btn btn-default'>Refresh Token</a><a class='btn btn-default'>View Gallery</a></td>");
 
-      $('#addr'+i).html("<td>"+ i +"</td><td><input id='skcontainer"+i+"' name='serialkey"+i+"' type='text' placeholder='Serial Key' class='form-control input-md' disabled/> </td><td><input id='namecontainer"+i+"' name='name"+i+"' type='text' placeholder='Name'  class='form-control input-md' disabled></td> <td style='vertical-align:middle' align='center'><a class='btn btn-default'>Invalidate User</a><a class='btn btn-default'>Refresh Token</a><a class='btn btn-default'>View Gallery</a></td>");
+        $("#skcontainer"+i).val(contents);
 
-      $("#skcontainer"+i).val(contents);
+        $("#namecontainer"+i).val(name);
 
-      $("#namecontainer"+i).val(name);
-
-      $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
-      
-      $("#skinput").val('');
-      $("#nameinput").val('');
-      i++; 
+        $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+        
+        $("#skinput").val('');
+        $("#nameinput").val('');
+        i++;
+      } 
     });
 
 
