aaaebfd012167286faa92c6256a4c38376358136
Giles Lavelle <giles.lavelle@gmail.com>
2014-03-15 22:04:42 +0000

    more metadata tests

--- test/specs/metadata.js
+++ test/specs/metadata.js
@@ -7,5 +7,30 @@ describe('Metadata', function(){
         it('should return a value', function(){
             md.isoString().should.be.defined;
         });
+
+        it('should have the correct length', function(){
+            md.isoString().should.have.length(24);
+        });
+    });
+
+    describe('title', function(){
+        it('should have a default title', function(){
+            md.get('title').should.equal('Untitled');
+        });
+
+        it('should allow the title to be updated', function() {
+            md.set('title', 'Giraffe');
+            md.get('title').should.equal('Giraffe');
+        });
+    });
+
+    describe('datetime', function(){
+        it('should have a default timestamp', function() {
+            md.get('datetime').should.be.defined;
+        });
+
+        it('should be a Date object', function() {
+            md.get('datetime').should.be.a('Date');
+        });
     });
 });
