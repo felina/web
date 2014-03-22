121ffcc75a6e342d4dfb6252d9caba110ab10289
Giles Lavelle <giles.lavelle@gmail.com>
2014-03-22 12:24:30 +0000

    browserify main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+var fl = require('./shared/common');
+var api = require('felina-js');
+
 $(function(){
+    fl.onPageLoad('index');
+
     var images = ['elephant', 'giraffe', 'leopard'];
 
     var randomImage = function(){
@@ -11,13 +16,11 @@ $(function(){
         'background-image': randomImage()
     });
 
-    fl.setSwitcherIcon('index');
-
     banner.find('button').on('click', function(){
         var data = {
             email: banner.find('#email').val(),
             pass: banner.find('#password').val()
         };
-        fl.login('login', data);
+        api.login('login', data);
     });
 });
