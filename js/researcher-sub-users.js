c7bdb5e3bf4aa342efeaec063fe459397bfcaca5
Andy <narayn60@gmail.com>
2014-03-23 17:29:41 +0000

    Before re-factor

--- js/researcher-sub-users.js
+++ js/researcher-sub-users.js
@@ -1,49 +1,92 @@
 var fl = require('./shared/common');
 var api = require('felina-js')();
 
+var ListElement = Backbone.View.extend({
+  render: function() {
+    
+  },
+});
+
+
+
+
+
 $(function(){
 
-
-    // $("#delete_row").click(function(){
-    //    if(i>1){
-    //     $("#addr"+(i-1)).html('');
-    //     i--;
-    //   }
-    // });
-
-
+    
+    // Get's the current subusers from the system.
     var users;
     var registered_subusers;
     api.get('subuser', function (data) {
       users = data;
-      console.log(JSON.stringify(users));
+      // console.log(JSON.stringify(users));
       if (users.res) {
         registered_subusers = users.subusers;
-      }
+        for (var a = 0; a < registered_subusers.length; a++) {
+          
+          var c = a + 1;
+          var contents = registered_subusers[a].email; //THE SERIAL KEY
+          var name = registered_subusers[a].name; //THE NAME
+          var projects = registered_subusers[a].projectid; //THE PROJECTS
+          var invalid = registered_subusers[a].invalidated; //1 if validated -1 if invalidated
+          var b = c;
+
+          $('#addr'+c).html(JST.subusers_element(
+              {c: b,
+                projects: projects}
+            ));
+
+          if (invalid) {
+            $("#tablenu"+b).css("background-color","red");
+          } else {
+            $("#tablenu"+b).css("background-color","green");
+          }
 
 
-      for (var a = 0; a < registered_subusers.length; a++) {
-        
-        var c = a + 1;
-        var contents = registered_subusers[a].email; //THE SERIAL KEY
-        var name = registered_subusers[a].name; //THE NAME
-        var projects = registered_subusers[a].projectid; //THE PROJECTS
+          //Get the value from the serial key input
+          $("#skcontainer"+b).val(contents);
+
+          //Get the value from the name input
+          $("#namecontainer"+b).val(name);
+
+          // Refresh the user
+          $("#refresh"+b).click(function(){
+            var newUser = {
+              "email": contents,
+              "refresh": 1 
+            };
+
+            api.post('updatesub', newUser, function (data) {
+              if (data.res) {
+                //Give info about successful refresh
+              }
+            });
+          });
+
+          //Invalidate the user
+          $("#invalidate"+b).click(function(){
+            var x = b;
+            var newUser1 = {
+              "email": contents,
+              "refresh": -1 
+            };
+
+            api.post('updatesub', newUser1, function (data) {
+              console.log(JSON.stringify(data));
+              if (data.res) {
+                //Give info about successful invalidate
+                $("#tablenu"+x).css("background-color","yellow");
+              }
+            });
+          });
 
 
-        var b = c;
+          $("#edit"+b).click(function(){
+              $("#namecontainer"+b).removeAttr("readonly");
+          });
 
-        $('#addr'+c).html("<td>"+ c +"</td><td><input id='skcontainer"+c+"' name='serialkey"+c+"' type='text' placeholder='Serial Key' class='form-control input-md' disabled/> </td><td><input id='namecontainer"+c+"' name='name"+c+"' type='text' placeholder='Name'  class='form-control input-md' disabled></td><td style='verticle-align:middle' align='center'><a class='btn btn-default'>"+projects+"</a></td> <td style='vertical-align:middle' align='center'><a class='btn btn-default'>Invalidate User</a><a class='btn btn-default'>Refresh Token</a><a class='btn btn-default'>View Gallery</a><a id='edit"+c+"' class='btn btn-default' data-toggle='button'>Edit</a></td>");
-
-        $("#skcontainer"+c).val(contents);
-
-        $("#namecontainer"+c).val(name);
-
-        $("#edit"+b).click(function(){
-            $("#namecontainer"+b).removeAttr("disabled");
-        });
-
-
-        $('#tab_logic').append('<tr id="addr'+(c+1)+'"></tr>');
+          $('#tab_logic').append('<tr id="addr'+(c+1)+'"></tr>');
+        }
       }
     });
 
@@ -55,9 +98,6 @@ $(function(){
     }
 
 
-    
-
-
     // Adds the user to the below table
     $("#add_user").click(function(){
       var i = $("#tab_logic > tbody > tr").length;
@@ -65,34 +105,53 @@ $(function(){
       var name = $("#nameinput").val(); //THE NAME
       var projects = $('#projectselect').val(); //THE PROJECTS
 
+      // api.post('subuser', function (data) {}
       // Makes sure that there is values added in the three fields
       if (contents && name && projects) {
 
-        // Send info to server
-        var e = i;
+
+        // JSON object to send to server
+        var newUser = {
+          "name": name,
+          "email": contents,
+          "projectid": 1
+        };
+
+         api.post('subuser', newUser, function (data) {
+            if (data.res) {
+              // Send info to server
+              var e = i;
 
 
-        //ADD EDIT AND TOGGLE TO SUBMIT AND CANCEL BUTTONS
-        $('#addr'+i).html("<td>"+ i +"</td><td><input id='skcontainer"+i+"' name='serialkey"+i+"' type='text' placeholder='Serial Key' class='form-control input-md' disabled/> </td><td><input id='namecontainer"+i+"' name='name"+i+"' type='text' placeholder='Name'  class='form-control input-md' disabled></td><td style='verticle-align:middle' align='center'><a class='btn btn-default'>"+projects+"</a></td> <td style='vertical-align:middle' align='center'><a class='btn btn-default'>Invalidate User</a><a class='btn btn-default'>Refresh Token</a><a class='btn btn-default'>View Gallery</a><a id='edit"+i+"' class='btn btn-default' data-toggle='button'>Edit</a></td>");
+              //ADD EDIT AND TOGGLE TO SUBMIT AND CANCEL BUTTONS
+              $('#addr'+i).html(JST.subusers_element(
+                {c: i,
+                projects: projects}
+              ));
 
-        $("#skcontainer"+i).val(contents);
+              $("#skcontainer"+i).val(contents);
 
-        $("#namecontainer"+i).val(name);
+              $("#namecontainer"+i).val(name);
 
-        $("#edit"+e).click(function(){
-            $("#namecontainer"+e).removeAttr("disabled");
-            console.log(i);
-        });
+              $("#edit"+e).click(function(){
+                  $("#namecontainer"+e).removeAttr("readonly");
+                  console.log(i);
+              });
 
 
-        $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+              $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
 
-        $("#skinput").val('');
-        $("#nameinput").val('');
-        
-        multiselect_deselectAll($("#projectselect"));
+              $("#skinput").val('');
+              $("#nameinput").val('');
+              
+              multiselect_deselectAll($("#projectselect"));
 
-        i++;
+              i++;
+            }
+            // Add clause if users is already defined etc........
+
+
+         });
       }
     });
 
