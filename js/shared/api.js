2e6d9d27e8cc216d27cc94e1cb6d48bb14805df2
Giles Lavelle <giles.lavelle@gmail.com>
2014-03-30 13:19:21 +0100

    back to s3

--- js/shared/api.js
+++ js/shared/api.js
@@ -1,3 +1,3 @@
 var API = require('felina-js');
-// window.api = new API();
-window.api = new API('http://nl.ks07.co.uk:5005/');
+window.api = new API();
+// window.api = new API('http://nl.ks07.co.uk:5005/');
