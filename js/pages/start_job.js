143b4371cf240ca4f2719208c22d361b874eb55b
Giles Lavelle <giles.lavelle@gmail.com>
2014-04-09 18:21:12 +0100

    get selected images from page

--- js/pages/start_job.js
+++ js/pages/start_job.js
@@ -1,23 +1,19 @@
 var onPageLoad = require('../shared/pageload');
-var FLImage = require('../models/image');
-var Thumbnail = require('../views/thumbnail');
 var Executable = require('../views/executable');
+var Gallery = require('../views/gallery');
 
 $(function(){
     onPageLoad('start_job');
 
-    var gallery = $('#gallery');
+    var gallery = new Gallery({});
+    gallery.render('#gallery');
 
     api.getImages(function(data) {
         if (data.res) {
             _.each(data.images, function(img) {
-                var image = new FLImage({
+                gallery.add({
                     src: api.url + 'img?id=' + img.imageid
                 });
-                var thumb = new Thumbnail({
-                    model: image
-                });
-                thumb.render().$el.appendTo(gallery);
             });
         }
     });
@@ -44,14 +40,19 @@ $(function(){
     });
 
     $('#start').on('click', function() {
-        console.log($('input[type=radio]:checked'));
-
         var exe = _.find(exes, function(e) { return e.selected; });
         if (!exe) {
             alert('Please choose an executable');
             return;
         }
-        var images = ['b3639a00751454b1fa0f0cc39fb5992c', '11d70a64fee30a8dacb58f7bdfcf25f3'];
+
+        var images = gallery.getSelected().map(function(img) {
+            return img.get('metadata').get('id');
+        });
+        if (images.length === 0) {
+            alert('Please choose at least one image');
+            return;
+        }
 
         api.startJob(exe.id, images, function(data) {
             console.log(data);
