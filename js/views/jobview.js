93769cc9e55563f4eb8c3ed8dced410cd05482b3
Giles Lavelle <giles.lavelle@gmail.com>
2014-05-05 17:47:01 +0100

    pause resume job button

--- js/views/jobview.js
+++ js/views/jobview.js
@@ -4,9 +4,19 @@ module.exports = Backbone.View.extend({
 
         if (this.model.get('completed')) {
             this.$('.progress').hide();
+            this.$('button').hide();
             this.$('#results').show();
         }
 
         return this;
+    },
+    events: {
+        'click button': 'togglePause'
+    },
+    togglePause: function() {
+        this.$('button i')
+            .toggleClass('glyphicon-play')
+            .toggleClass('glyphicon-pause');
+        this.$('.progress').toggleClass('active');
     }
 });
