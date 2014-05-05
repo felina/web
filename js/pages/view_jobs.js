4e1af23bfcf584ce2a69b1ccde5f68eaf73a3989
Giles Lavelle <giles.lavelle@gmail.com>
2014-05-05 13:01:58 +0100

    improve view jobs page

--- js/pages/view_jobs.js
+++ js/pages/view_jobs.js
@@ -3,14 +3,59 @@ var onPageLoad = require('../shared/pageload');
 $(function(){
     onPageLoad('view_jobs');
 
-    var jobs = $('.jobs');
+    var lists = {
+        queued: $('#queued'),
+        active: $('#active'),
+        completed: $('#completed')
+    };
 
-    // Get data for the jobs and add it to the DOM
+    // Get all jobs started by the current user
     api.getJobs(function(data) {
-        if (data.res) {
-            _.each(data.jobs, function(job){
-                jobs.append(JST.job(job));
-            });
+        console.log(data);
+
+        // Show an error if the jobs failed to load
+        if (!data.res) {
+            alert('Failed to load jobs', 'bad');
+            return;
         }
+
+        var jobs = data.jobs;
+
+        // Split the jobs in to three categories, queued, active and completed
+        var types = {
+            queued: _.filter(jobs, function(job) { return !job.started; }),
+            active: _.filter(jobs, function(job) { return job.started && !job.completed; }),
+            completed: _.filter(jobs, function(job) { return job.completed; })
+        };
+
+        // Handle each category in turn
+        _.each(['queued', 'active', 'completed'], function(type) {
+            var t = types[type];
+            var el = lists[type];
+
+            // If the current category has no jobs, display a message in this list
+            if(t.length === 0) {
+                el.text('No ' + type + ' jobs.');
+                return;
+            }
+
+            // Otherwise, add all the jobs in this category to the category's list
+            _.each(t, function(job){
+                // For completed jobs, get the URL of the CSV file containg the results of the job
+                if (type === 'completed') {
+                    api.getCSV(job.jobid, function(data) {
+                        if (data.res) {
+                            job.csvurl = data.url;
+                            el.append(JST.job(job));
+                        }
+                        else {
+                            alert('Failed to load job results for job ' + job.name, 'bad');
+                        }
+                    });
+                }
+                // For other types of job, just add it straight to the DOM
+                el.append(JST.job(job));
+            });
+        });
     });
 });
