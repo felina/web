3bcd6d1753e33edc687217c034c0ee9104e7a580
Andy <narayn60@gmail.com>
2014-05-05 14:46:53 +0100

    Added dummy photos

--- js/pages/user_profile/photos/user_photos.js
+++ js/pages/user_profile/photos/user_photos.js
@@ -2,15 +2,15 @@ $(function(){
     var profTabWrap = $('.profile_tab');
 
     var photos = [
-        '/img/shutter.png',
-        '/img/shutter.png',
-        '/img/shutter.png',
-        '/img/shutter.png',
-        '/img/shutter.png',
-        '/img/shutter.png',
-        '/img/shutter.png',
-        '/img/shutter.png',
-        '/img/shutter.png',
+        'http://hd.wallpaperswide.com/thumbs/lion_5-t2.jpg',
+        'http://3.bp.blogspot.com/-Fo7AHLKOH5M/T8WeqUcZFKI/AAAAAAAAFCc/cQdII_3xu7k/s1600/sloth.jpg',
+        'http://4.bp.blogspot.com/-yiqFMaq08ZA/UDko5YqnGGI/AAAAAAAAJgs/PSGs0pFjZNQ/s1600/black_rhino+1.jpg',
+        'http://upload.wikimedia.org/wikipedia/commons/8/81/2012_Suedchinesischer_Tiger.JPG',
+        'http://www.thetimes.co.uk/tto/multimedia/archive/00224/51989793_SHark_224346c.jpg',
+        'http://www.nmfs.noaa.gov/pr/images/fish/mantaray_fgbnms_noaa.jpg',
+        'http://upload.wikimedia.org/wikipedia/commons/0/07/Emperor_Penguin_Manchot_empereur.jpg',
+        'http://rack.2.mshcdn.com/media/ZgkyMDE0LzA0LzE4LzdkL0F3a3dhcmRTZWFsLjljMzdlLmpwZw/1a318fd2/ecf/AwkwardSeal.jpg',
+        'http://upload.wikimedia.org/wikipedia/commons/a/af/Honey_badger.jpg',
     ];
 
     //Gets images that are then displayed in the user photo gallery on the main page
